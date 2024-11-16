5b44cd8aa64dfc96fec0074c9382004690ac0cc0
Kyle Owen <kkowen88@gmail.com>
2024-11-16 00:37:20 -0500

    ghost scene with movement

--- oppositesattract/scripts/ghostMovement.gd
+++ oppositesattract/scripts/ghostMovement.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 150
 
 func get_input():
 	var input_direction = Input.get_vector("ghost left", "ghost right", "ghost up", "ghost down")
@@ -10,6 +10,6 @@ func _physics_process(_delta):
 	get_input()
 	move_and_slide()
 	if velocity.x > 0:
-		$AnimatedSprite2D.flip_h = true;
-	if velocity.x < 0:
 		$AnimatedSprite2D.flip_h = false;
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true;
