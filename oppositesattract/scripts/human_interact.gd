64c71a2981d5dd33ffe10b0618e6d6ed4c0ab673
Gibril <gabrielhwspam@gmail.com>
2024-11-16 00:53:56 -0500

    popup should toggle on enter area

---
+++ oppositesattract/scripts/human_interact.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@onready var e: Sprite2D = $E
+@onready var q: Sprite2D = $Q
+
+func _on_body_entered(body: Node2D) -> void:
+	e.visible = not e.visible
+	q.visible = not q.visible
+
+
+func _on_body_exited(body: Node2D) -> void:
+	e.visible = not e.visible
+	q.visible = not q.visible
