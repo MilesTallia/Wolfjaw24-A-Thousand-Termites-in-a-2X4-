1a2e43829988d290c8523a2569f3eab68452100d
Andrew DiMauro <asd9216@g.rit.edu>
2024-11-16 17:24:23 -0500

    doors should work and circuit breakers

--- oppositesattract/scenes/objects/metal_door.gd
+++ oppositesattract/scenes/objects/metal_door.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
 @onready var collision_shape_2d: CollisionShape2D = $AnimatableBody2D/CollisionShape2D
+@onready var sprite_2d: Sprite2D = $AnimatableBody2D/Sprite2D
 
 
 func open():
 	collision_shape_2d.disabled = true
+	sprite_2d.visible = false
+
 	
 func close():
 	collision_shape_2d.disabled = false
+	sprite_2d.visible = true
