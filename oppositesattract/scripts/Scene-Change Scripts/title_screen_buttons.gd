948d25151ab74980c1c9d6f57dd91a123585787b
Miles Tallia (RIT Student) <mot7506@rit.edu>
2024-11-16 14:17:25 -0500

    instructions page

--- oppositesattract/scripts/Scene-Change Scripts/title_screen_buttons.gd
+++ oppositesattract/scripts/Scene-Change Scripts/title_screen_buttons.gd
@@ -3,6 +3,8 @@ extends Control
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Title_Screen/credits.tscn")
+
+func _on_instructions_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Title_Screen/instructions.tscn")
