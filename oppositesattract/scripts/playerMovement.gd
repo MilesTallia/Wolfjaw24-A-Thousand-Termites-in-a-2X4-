2542d5acac52d1436b226afc25ed9c54ab297012
Miles Tallia (RIT Student) <mot7506@rit.edu>
2024-11-15 23:06:52 -0500

    movements

---
+++ oppositesattract/scripts/playerMovement.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("human left", "human right", "human up", "human down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = true;
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = false;
