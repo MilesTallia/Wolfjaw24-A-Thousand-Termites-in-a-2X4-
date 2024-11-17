1f437121a9d87a667a3f644f59b0910c545dbb89
Gibril <gabrielhwspam@gmail.com>
2024-11-16 19:04:23 -0500

    fixed levers and breakers

--- oppositesattract/scripts/human_interact.gd
+++ oppositesattract/scripts/human_interact.gd
@@ -4,10 +4,12 @@ extends Node
 @onready var q: Sprite2D = $Q
 
 func _on_body_entered(body: Node2D) -> void:
-	e.visible = not e.visible
-	q.visible = not q.visible
+	if body.is_in_group("players"):
+		e.visible = not e.visible
+		q.visible = not q.visible
 
 
 func _on_body_exited(body: Node2D) -> void:
-	e.visible = not e.visible
-	q.visible = not q.visible
+	if body.is_in_group("players"):
+		e.visible = not e.visible
+		q.visible = not q.visible
