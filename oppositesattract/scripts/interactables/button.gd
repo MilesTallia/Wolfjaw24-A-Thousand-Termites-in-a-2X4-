70a13a2b9832fc3affc8cc4d1bb5a76f4972ec35
Andrew DiMauro <asd9216@g.rit.edu>
2024-11-16 20:12:17 -0500

    button implmentation

--- oppositesattract/scripts/interactables/button.gd
+++ oppositesattract/scripts/interactables/button.gd
@@ -1,30 +1,45 @@
 extends Node2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var metal_door: Node2D = $"../MetalDoor"
+@export var door_array: Array[Node2D] = []
+@export var num_time: int
+@onready var timer: Timer = $Timer
+var counter: int = 0
 
 var in_range = false
 
 func _input(event):
 	if in_range and Input.is_action_just_pressed("interact"):
-		if animated_sprite_2d.frame == 1:
-			off()
-		elif animated_sprite_2d.frame == 0:
+		if animated_sprite_2d.frame == 0:
 			on()
 
 func on():
 	animated_sprite_2d.frame = 1
-	metal_door.open()
+	timer.start()
+	for door in door_array:
+		door.toggle()
 		
 func off():
 	animated_sprite_2d.frame = 0
-	metal_door.close()
+	for door in door_array:
+		door.toggle()
+		
+func toggle():
+	if animated_sprite_2d.frame == 0:
+		on()
 	
-
 
 func _on_button_prompt_body_entered(body: Node2D) -> void:
 	in_range = true
 
-
 func _on_button_prompt_body_exited(body: Node2D) -> void:
 	in_range = false
+
+
+func _on_timer_timeout() -> void:
+	counter += 1
+	if counter >= num_time:
+		off()
+		counter = 0
+		timer.stop()
+		
